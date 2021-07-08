2a2652dcf2789cd4eed27675fe421943abe2eaca
Patrick Genier <patrick.j.genier@gmail.com>
2021-07-08 15:16:47 -0400

    7-8-21-2ndCom

---
+++ electriccalc.js
@@ -0,0 +1,34 @@
+//------//
+function ohmsCalc() {
+    /*Ohms*/
+    var E = document.getElementById("myVolts1").value;
+  
+    var I = document.getElementById("myAmps1").value;
+ 
+    R = E / I;
+
+    /*Amps*/
+    document.getElementById("myResOut").innerHTML = R.toFixed(1);
+}
+function ampsCalc() {
+    /*Amps*/
+    var E = document.getElementById("myVolts2").value;
+
+    var R = document.getElementById("myRes2").value;
+
+    I = E / R;
+    
+    document.getElementById("myAmpsOut").innerHTML = I.toFixed(1);
+}
+
+function voltsCalc() {
+
+    var I = document.getElementById("myAmps3").value;
+
+    var R = document.getElementById("myRes3").value;
+
+    E = I * R;
+
+    document.getElementById("myVoltsOut").innerHTML = E.toFixed(1);
+
+}
